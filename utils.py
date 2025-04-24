17dd118f97ef8e0fa0c44bd441ac6591941d3c64
Priyadarshi18 <kumarpriyadarshid@gmail.com>
2025-04-24 18:11:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,15 +3,24 @@ import numpy as np
 import base64
 import io
 import fitz
+import requests
 from PIL import Image
 import google.generativeai as genai
 
 # === Gemini Config ===
 genai.configure(api_key="AIzaSyC1MFhWhGuf0Nxl5uT8eJYnClBxMWI78OA")
 
-def remove_shadow_opencv(image_path):
-    img = cv2.imread(image_path)
-    rgb_planes = cv2.split(img)
+def download_image_from_url(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ValueError("Image URL could not be downloaded.")
+    img_bytes = io.BytesIO(response.content)
+    return Image.open(img_bytes).convert("RGB")
+
+def remove_shadow_opencv_from_image(pil_img):
+    cv_img = np.array(pil_img)
+    cv_img = cv_img[:, :, ::-1].copy()  
+    rgb_planes = cv2.split(cv_img)
     result_planes = []
     for plane in rgb_planes:
         dilated = cv2.dilate(plane, np.ones((7, 7), np.uint8))
@@ -20,7 +29,7 @@ def remove_shadow_opencv(image_path):
         norm = cv2.normalize(diff, None, 0, 255, cv2.NORM_MINMAX)
         result_planes.append(norm)
     result = cv2.merge(result_planes)
-    return Image.fromarray(result)
+    return Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 
 def convert_pdf_to_image(pdf_path):
     doc = fitz.open(pdf_path)
